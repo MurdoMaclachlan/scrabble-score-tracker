49e6a199beab6169f601cf38c244d067474b642c
Murdo Maclachlan <murdomaclachlan@gmail.com>
2020-03-25 12:01:45 +0000

    Added commenting

--- libsst/setup.py
+++ libsst/setup.py
@@ -2,9 +2,14 @@ from flask import request
 from .classes import Players
 
 def doSetup(players, i):
+
+    #gets player data
     name = request.form["name"]
     colour = request.form["colour"]
     score = 0
+
+    #declares instance of the "Players" class and adds it to array of records
     y = Players(name, colour, score, i)
     players.append(y)
+
     return players
